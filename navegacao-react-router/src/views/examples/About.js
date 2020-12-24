a0e21ac935ebf8f61daa04b3accc0aa2f51af743
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-24 17:48:38 -0300

    Estrutura do projeto

---
+++ navegacao-react-router/src/views/examples/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const About = props =>{
+    <aside className="About">
+        <h1>Conponente About</h1>
+        <p>O nosso sistema foi criado com sucesso</p>
+
+    </aside>
+}
+
+
+export default About
---
+++ navegacao-react-router/src/views/examples/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Home = props =>{
+    <aside className="Home">
+        <h1>Inicio</h1>
+        <p>Bem vindo</p>
+
+    </aside>
+}
+
+
+export default Home
